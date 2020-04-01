03a62baf765e7769000d48484e78acefa1413621
Ikram Shah <ikram2shah@gmail.com>
2020-04-02 01:56:39 +0530

    Socket dependency addition

--- sockets/read-all-users.js
+++ sockets/read-all-users.js
@@ -6,23 +6,20 @@ const client = new Client({
 })
 
 async function getUsers() {
-    // console.log('Function `read-all-users` invoked')
     try {
         const response = await client.query(q.Paginate(q.Match(q.Ref('indexes/all_users'))))
         const userRefs = response.data
-        // console.log('User refs', userRefs)
-        // console.log(`${userRefs.length} users found`)
         const getAllUserDataQuery = userRefs.map((ref) => {
             return q.Get(ref)
         })
         const ret = await client.query(getAllUserDataQuery)
-        console.log(ret.sort(function(a, b) {
+        ret.sort(function(a, b) {
             return new Date(b['data']['lastUpdated']) - new Date(a['data']['lastUpdated']);
-        }))
+        })
         return ret
     }
     catch (error) {
-        console.log('error', error)
+        console.log('ERROR', error)
         return error
     }
 }
